93f228dad803396ee6deddd5e6f5531b6e1c503e
shredpap <75381351+shredpap@users.noreply.github.com>
2022-10-16 23:58:31 +0545

    update Q5

--- q5.py
+++ q5.py
@@ -5,15 +5,25 @@
 # groups there will be, and how many students will be in the smaller "left over"
 # group.
 l=24
-d,e,f=113,175,12
-def numned(labgrp,a,b,c):
-    sub=0
-    sub+=a%labgrp
-    sub+=b%labgrp
-    sub+=c%labgrp
-    
-    print(a//labgrp)                                        k
-    print(b//labgrp)
-    print(c//labgrp)
-    print((sub+c)//labgrp)
-numned(l,d,e,f)
+list=[113,175,12]
+def main(a,l=24):
+    print("class with",a,"students will have",a//l,"groups")
+# def sub(a,l=24):
+    # sub=0
+    # sub+=i%labgrp
+    # sub+=b%labgrp
+    # sub+=c%labgrp
+    # print(sub//l)
+for i in range(len(list)):
+    main(list[i])
+    # sub(list[i])
+
+sub=0
+sub+=list[0]%l
+sub+=list[1]%l
+sub+=list[2]%l
+print("leftover group will have",sub,"students",)
+# print("total groups needed: ",)
+if sub>l:
+    print("smaller leftover group will have",sub-l,"students")
+
